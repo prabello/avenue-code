1c4cc687538e5b3ca8b1ae425e8f2d0933fb65b0
Pedro <pedrommrabello@gmail.com>
2016-12-03 08:59:37 -0200

    Project creation, spring boot initialized

---
+++ src/main/java/com/avenuecode/AvenueApplication.java
@@ -0,0 +1,12 @@
+package com.avenuecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvenueApplication.class, args);
+	}
+}
